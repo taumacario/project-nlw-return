d7c51bccc08faf648c073684b90ff534baed98e0
Tauana Macario <taumacario@gmail.com>
2022-05-04 19:19:49 -0300

    stage03

---
+++ index.js
@@ -0,0 +1,21 @@
+function onScroll (){
+  if (scrollY > 0){
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu (){
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu (){
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin:'top',
+  distance: '30px',
+  duration: 900,
+}).reveal('#home, #home img, #home .stats, #services, #services header, #services .card, #about, #about header, #about .content')
